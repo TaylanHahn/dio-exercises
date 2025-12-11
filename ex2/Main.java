5b5a5fc3c8ba125d98f56fb93a3b39a976665ea7
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-12-10 21:30:51 -0300

    Add files via upload

---
+++ ex2/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String codigo = scanner.nextLine();
+        
+        if(codigo.equals("EC2")){
+          System.out.println("EC2");
+        } else if (codigo.equals("S3")){
+          System.out.println("S3");
+        } else if (codigo.equals("Lambda")){
+          System.out.println("Lambda");
+        } else {
+          System.out.println("Codigo invalido");
+        }
+
+        // TODO: Verifique se o código corresponde exatamente a "EC2", "S3" ou "Lambda" e imprima o serviço correspondente.
+        // Caso contrário, imprima "Codigo invalido".
+    }
+}
