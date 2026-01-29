c428c9f02bcc0f82bb166634c7f0f8f915bc0dd5
Taylan <taylan.hahn@gmail.com>
2026-01-29 04:36:53 -0300

    sudoku em java

---
+++ src/stream_api/sudoku/model/GameStatusEnum.java
@@ -0,0 +1,21 @@
+package stream_api.soduko.model;
+
+public enum GameStatusEnum {
+    NON_STARTED("não iniciado"),
+    INCOMPLETE("incompleto"),
+    COMPLETE("completo");
+
+    private String label;
+
+    GameStatusEnum(String s) {
+    }
+
+    void GameStatusEnum(final String label){
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+}
