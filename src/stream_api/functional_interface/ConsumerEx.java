722552c278894004133dc4e47a56613041c1b058
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2026-01-29 01:45:45 -0300

    Update ConsumerEx.java

--- src/stream_api/functional_interface/ConsumerEx.java
+++ src/stream_api/functional_interface/ConsumerEx.java
@@ -15,7 +15,7 @@ public class ConsumerEx {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
         // Usar o Consumer com expressão lambda para imprimir números pares
-        // Lambda: "argumento => corpo"
+        // Lambda: "argumento -> corpo"
         Consumer<Integer> imprimirNumeroPar = numero -> {
             if (numero % 2 == 0) {
                 System.out.println(numero);
